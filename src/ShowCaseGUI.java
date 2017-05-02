0db2cb1a5ad1fbaf739c90ff3c5b7a329093268b
Anas <Anas@192.168.0.3>
2017-05-01 21:56:29 -0700

    gui btns

--- src/ShowCaseGUI.java
+++ src/ShowCaseGUI.java
@@ -3,6 +3,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JButton;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 public class ShowCaseGUI {
 
@@ -38,9 +45,18 @@ public class ShowCaseGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		JButton btnNewButton = new JButton("Easy");
+		btnNewButton.setBounds(10, 227, 109, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Medium");
+		btnNewButton_1.setBounds(165, 227, 109, 23);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Hard");
+		btnNewButton_2.setBounds(315, 227, 109, 23);
+		frame.getContentPane().add(btnNewButton_2);
 	}
-
 }
