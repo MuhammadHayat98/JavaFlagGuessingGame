0c78f837887868a9ce408f540b9d648c0eb5639b
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2017-05-12 16:54:51 -0700

    changed counter

--- src/Difficulty.java
+++ src/Difficulty.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 
 public class Difficulty extends ShowCaseGUI{
@@ -14,8 +15,14 @@ public class Difficulty extends ShowCaseGUI{
 		/*Generating random image code goes here
 		 * 
 		 */
-		
-		ImageIcon image = new ImageIcon(array[0][1]);
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setMaximum(10);
+		progressBar.setValue(9);
+		progressBar.setBounds(135, 224, 250, 22);
+		picFrame.getContentPane().add(progressBar);
+		FlagGenerator flagGen = new FlagGenerator(3);
+		int flag = flagGen.getFlag();
+		ImageIcon image = new ImageIcon(array[flag][1]);
 		picPanel.setBounds(35, 11, 360, 150);
 		picFrame.getContentPane().add(picPanel);
 		JLabel lblNewLabel_1 = new JLabel(image);
@@ -35,10 +42,9 @@ public class Difficulty extends ShowCaseGUI{
 		
 			public void actionPerformed(ActionEvent arg0) {
 				
-				int flag;
-				int count=0;
+				
 				String guess = textField1.getText();
-				if(guess.equalsIgnoreCase(array[0][0])) {
+				if(guess.equalsIgnoreCase(array[flag][0])) {
 					System.out.print("noicew");
 				lblNewLabel.setIcon(new ImageIcon("Icons/rsz_checkmark-xxl.png"));
 				}
@@ -59,6 +65,11 @@ public class Difficulty extends ShowCaseGUI{
 		/*Generating random image code goes here
 		 * 
 		 */
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setMaximum(10);
+		progressBar.setValue(9);
+		progressBar.setBounds(135, 224, 250, 22);
+		picFrame.getContentPane().add(progressBar);
 		FlagGenerator flagGen = new FlagGenerator(2);
 		int flag = flagGen.getFlag();
 		ImageIcon image = new ImageIcon(array[flag][1]);
@@ -82,28 +93,28 @@ public class Difficulty extends ShowCaseGUI{
 		int count = 10;	
 			public void actionPerformed(ActionEvent arg0) {
 				
-				Label label = new Label(count + " tries remain");
+				
 				
 				
 				String guess = textField1.getText();
 				if(guess.equalsIgnoreCase(array[flag][0])) {
 					int tries = count+1;
-					//Label label = new Label("It only took you " + tries + " tries, now here's another");
-					//label.setBounds(135, 224, 250, 22);
-					//picFrame.getContentPane().add(label);
+					picFrame.remove(progressBar);
+					Label label = new Label("It only took you " + tries + " tries, now here's another");
+					label.setBounds(135, 224, 250, 22);
+					picFrame.getContentPane().add(label);
 				lblNewLabel.setIcon(new ImageIcon("Icons/rsz_checkmark-xxl.png"));
 				}
 				else
 				{
+					
+					progressBar.setValue(count);
 					count--;
-					picFrame.getContentPane().remove(label);
-					 label = new Label(count + " tries remain");
 					lblNewLabel.setIcon(new ImageIcon("Icons/rsz_milker-x-icon.png"));
 				}
 				System.out.println(count);
 				
-				label.setBounds(135, 224, 156, 22);
-				picFrame.getContentPane().add(label);
+				
 			}
 		});
 		
